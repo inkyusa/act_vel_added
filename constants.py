fd881fd236ce668443d5af20b94fd4ca7bc4cea2
Inkyu Sa <enddl22@gmail.com>
2024-01-11 08:06:07 +1100

    Auto stash before merge of "main" and "origin/main"

--- constants.py
+++ constants.py
@@ -3,14 +3,14 @@ import os
 
 ### Task parameters
 # DATA_DIR = '/home/zfu/interbotix_ws/src/act/data' if os.getlogin() == 'zfu' else '/scr/tonyzhao/datasets'
-DATA_DIR = '/home/inkyu/workspace/aloha_ds/sim'
+DATA_DIR = '/home/inkyu/workspace/act_private/data/sim'
 SIM_TASK_CONFIGS = {
     'sim_transfer_cube_scripted':{
-        'dataset_dir': DATA_DIR + '/sim_transfer_cube_scripted_gen_topcam',
+        'dataset_dir': DATA_DIR + '/sim_transfer_cube_scripted_3cams',
         'num_episodes': 50,
         'episode_len': 400,
-        #'camera_names': ['top', 'left_wrist', 'right_wrist']
-        'camera_names': ['top']
+        'camera_names': ['top', 'left_wrist', 'right_wrist']
+        #'camera_names': ['top']
     },
 
     'sim_transfer_cube_human':{
